433f9dc872dc5db2706ccb76fe82e4482e3274c0
z1rra <meelisen@gmail.com>
2025-01-10 09:47:18 +0200

    changed daterange

--- add_leave_test.ts
+++ add_leave_test.ts
@@ -19,10 +19,11 @@ Scenario('Employee request paid leave', ({ I }) => {
     //navigate to leaves
     I.click('Leaves');
     //fill out the leave request, only date fields are changed at the moment
+    //dates need to be changed to available range each time manually for now
     //TODO this should be a function if it needs to be reused
     I.click('//button[@aria-label="Request new leave"]');
-    I.click('//div[@class="day-cell" and text()="14"]');
-    I.click('//div[@class="day-cell" and text()="15"]');
+    I.click('//div[@class="day-cell" and text()="16"]');
+    I.click('//div[@class="day-cell" and text()="17"]');
     I.click('//button[@data-test="submit-approve"]');
     //confirm request in the modal
     I.click('Confirm');
